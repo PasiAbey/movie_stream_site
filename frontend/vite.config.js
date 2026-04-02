eef682bb690cc1323c8c38c893cbe59538c75bd1
Pasindu <pasindumadushan722@gmail.com>
2026-04-03 01:56:44 +0530

    Fixed Vite base path for AWS S3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Movie_streamer/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
